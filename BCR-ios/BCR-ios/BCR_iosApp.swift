252246889114d86b90491bc7cde612ea50ec3de0
Emma Walker <walkere@tcd.ie>
2022-09-11 19:11:03 +0100

    adds skeleton project.
    updates app icon and launch screen

---
+++ BCR-ios/BCR-ios/BCR_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCR_iosApp.swift
+//  BCR-ios
+//
+//  Created by Emma Walker on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BCR_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
